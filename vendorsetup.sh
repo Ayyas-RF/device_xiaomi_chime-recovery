5d625fe0ac9a5c4ae75a7d59c1c5067c33b43358
VikXL <berrzert9@gmail.com>
2026-05-02 16:23:58 +0700

    Fix indentation and edited personal info

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -18,7 +18,6 @@
 # 	Please maintain this if you use this script or any part of it
 #
 FDEVICE="chime"
-#set -o xtrace
 
 fox_get_target_device() {
 local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
@@ -37,7 +36,7 @@ fi
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
    	export TW_DEFAULT_LANGUAGE="en"
 	export LC_ALL="C"
-        export TARGET_DEVICE_ALT="citrus, lime, lemon, pomelo, juice, chime"
+    export TARGET_DEVICE_ALT="citrus, lime, lemon, pomelo, juice, chime"
  	export ALLOW_MISSING_DEPENDENCIES=true
 	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
 	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
@@ -54,32 +53,30 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export FOX_USE_TAR_BINARY=1
 	export FOX_USE_SED_BINARY=1
 	export FOX_USE_XZ_UTILS=1
-        export OF_QUICK_BACKUP_LIST="/boot;/dtbo;"
+    export OF_QUICK_BACKUP_LIST="/boot;/dtbo;"
 	export OF_PATCH_AVB20=1
 	export OF_DEFAULT_KEYMASTER_VERSION=4.1
-        export FOX_DELETE_AROMAFM=1
-        export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"; # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
+    export FOX_DELETE_AROMAFM=1
+    export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"; # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
 	export FOX_USE_NANO_EDITOR=1
 	export OF_IGNORE_LOGICAL_MOUNT_ERRORS=1
-        export OF_FBE_METADATA_MOUNT_IGNORE=1
+    export OF_FBE_METADATA_MOUNT_IGNORE=1
 
-        # OTA
-        export OF_KEEP_DM_VERITY=1
-        export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
-        export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
-        export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+    # OTA
+    export OF_KEEP_DM_VERITY=1
+    export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+    export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+    export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
 
 	# Screen settings
-        export OF_SCREEN_H=2340
-        export OF_STATUS_H=80
-        export OF_STATUS_INDENT_LEFT=48
-        export OF_STATUS_INDENT_RIGHT=48
+    export OF_SCREEN_H=2340
+    export OF_STATUS_H=80
+    export OF_STATUS_INDENT_LEFT=48
+    export OF_STATUS_INDENT_RIGHT=48
 	export OF_CLOCK_POS=1
 
 	# R11.3 Settings
-	export FOX_MAINTAINER_PATCH_VERSION="9"
-	export OF_MAINTAINER="itsHanibee"
-        export OF_MAINTAINER_AVATAR="device/xiaomi/chime/hani.png"
+	export OF_MAINTAINER="VikXL"
 
 	# let's see what are our build VARs
 	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
